1aceb1fe93abae7c9fbd67923323aea6884f671b
Lunichkin Matvey <ml@investprojects.info>
2026-03-03 19:03:42 +0300

    upd phar

--- bin/build-phar.php
+++ bin/build-phar.php
@@ -10,9 +10,14 @@ try {
     $phar = new Phar($pharFile, 0, $pharFile);
     $phar->setSignatureAlgorithm(Phar::SHA256);
     $phar->buildFromDirectory(__DIR__ . '/../', '/^(?!.*(?:\.git|tests|build-phar\.php|.*\.phar)).*$/i');
-    $phar->setDefaultStub('bin/console', 'bin/console');
+    
+    // Создаём заглушку с shebang
+    $stub = "#!/usr/bin/env php\n" . $phar->createDefaultStub('bin/console');
+    $phar->setStub($stub);
 
     echo "Phar-архив успешно создан: {$pharFile}\n";
+
+    @chmod($pharFile, 0755);
 } catch (Exception $e) {
     echo "Ошибка при создании phar: {$e->getMessage()}\n";
 }
